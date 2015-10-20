5d4dd7b3b86936c1cf00c238fd1681419457192b
Andrejs Tomsons <andrejs201@yahoo.com>
2015-10-20 15:24:54 -0400

    Work

--- cis237Inclass4/Program.cs
+++ cis237Inclass4/Program.cs
@@ -32,11 +32,36 @@ namespace cis237Inclass4
             Console.WriteLine();
             Console.WriteLine();
 
+            //Print out the 2nd one
+            Node nodeINeed = myLinkedList.Retrieve(2);
+            Console.WriteLine(nodeINeed.Data);
+
+            //Print out the second one again in one statement
+            Console.WriteLine(myLinkedList.Retrieve(2).Data);
+
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
             //Delete the 2nd element in the list
             myLinkedList.Delete(2);
             //Delete the new second element in the list. Was 3rd before previous delete
             myLinkedList.Delete(2);
 
+            for (Node x = myLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Add two new ones to the list
+            myLinkedList.Add("fifth");
+            myLinkedList.Add("sixth");
+
+            //Print the list one last time
             for (Node x = myLinkedList.Head; x != null; x = x.Next)
             {
                 Console.WriteLine(x.Data);
